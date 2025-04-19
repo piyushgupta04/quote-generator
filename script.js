117f5926b055dc11a635dabf6ffe5c7113242ab4
piyushgupta04 <normipiyush69@gmail.com>
2025-04-19 12:09:56 +0530

    added better gradient
    I guess so

--- script.js
+++ script.js
@@ -832,7 +832,7 @@ btn.addEventListener("click", () => {
 
 
 // const classRemover = () => {
-//   body.classList.remove("bg-gradient-to-tl", "from-[#f472b6]", "via-[#be185d]", "to-[#831843]")
+//   body.classList.remove("bg-gradient-to-tl", "from-[#831843]", "via-[#be185d]", "to-[#e23c92]")
 //   h1.classList.remove("text-pink-200")
 //   pDivChild[0].classList.remove("text-pink-200")
 //   pDivChild[1].classList.remove("text-pink-300")
@@ -856,7 +856,7 @@ btn.addEventListener("click", () => {
 // }
 
 const applyDarkMode = () => {
-  body.classList.remove("bg-gradient-to-tl", "from-[#f472b6]", "via-[#be185d]", "to-[#831843]")
+  body.classList.remove("bg-gradient-to-tl", "from-[#831843]", "via-[#be185d]", "to-[#e23c92]")
   h1.classList.remove("text-pink-200")
   pDivChild[0].classList.remove("text-pink-200")
   pDivChild[1].classList.remove("text-pink-300")
@@ -888,7 +888,7 @@ const applyLightMode = () =>{
   changer.children[0].remove();
   //adders
   changer.innerHTML += '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#831843"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z"/></svg>'
-  body.classList.add("bg-gradient-to-tl", "from-[#f472b6]", "via-[#be185d]", "to-[#831843]")
+  body.classList.add("bg-gradient-to-tl", "from-[#831843]", "via-[#be185d]", "to-[#e23c92]")
   h1.classList.add("text-pink-200")
   pDivChild[0].classList.add("text-pink-200")
   pDivChild[1].classList.add("text-pink-300")
